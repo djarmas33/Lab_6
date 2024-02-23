d22252d2ccc32f45c98d16a6a39bd7b5d714fd73
djarmas33 <113376942+djarmas33@users.noreply.github.com>
2024-02-23 12:19:21 -0500

    Add files via upload

---
+++ Lab 6.c
@@ -0,0 +1,78 @@
+//Array of numbers, two integers, low and high, representing indexes into the array
+
+#include <stdio.h>
+#include <stdlib.h>
+
+//find the index of numbers[value]
+int search(int numbers[], int low, int high, int value) 
+{
+    while (low <= high) {
+        int mid = low + (high - low) / 2;
+        
+        if (numbers[mid] == value) {
+            return mid; 
+        }
+        else if (numbers[mid] < value) {
+            low = mid + 1;
+        }
+        else {
+            high = mid - 1;
+        }
+    }
+    
+    return -1;
+}
+
+void printArray(int numbers[], int sz)
+{
+	int i;
+	printf("Number array : ");
+	for (i = 0;i<sz;++i)
+	{
+		printf("%d ",numbers[i]);
+	}
+	printf("\n");
+}
+
+int main(void)
+{
+	int i, numInputs;
+	char* str;
+	float average;
+	int value;
+	int index;
+	int* numArray = NULL;
+	int countOfNums;
+	FILE* inFile = fopen("input.txt","r");
+
+	fscanf(inFile, " %d\n", &numInputs);
+	
+	while (numInputs-- > 0)
+	{
+		fscanf(inFile, " %d\n", &countOfNums);
+		numArray = (int *) malloc(countOfNums * sizeof(int));
+		average = 0;
+		for (i = 0; i < countOfNums; i++)
+		{
+			fscanf(inFile," %d", &value);
+			numArray[i] = value;
+			average += numArray[i];
+		}
+
+		printArray(numArray, countOfNums);
+		value = average / countOfNums;
+		index = search(numArray, 0, countOfNums - 1, value);
+		if (index >= 0)
+		{
+			printf("Item %d exists in the number array at index %d!\n",value, index);
+		}
+		else
+		{
+			printf("Item %d does not exist in the number array!\n", value);
+		}
+
+		free(numArray);
+	}
+
+	fclose(inFile);
+}
